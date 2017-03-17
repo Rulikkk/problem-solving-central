db3280735e6c0b1cfe50ca56206ddafd7c145d4f
maximnezabotin <corleonesquad@gmail.com>
2017-03-17 16:31:27 +0300

    Update binarySearch.js

--- Gold standard/Sorting and searching/binarySearch.js
+++ Gold standard/Sorting and searching/binarySearch.js
@@ -1,49 +1,46 @@
-var MIN_INT_NUMBER = 0;
-var MAX_INT_NUMBER = 10;
-var ARRAY_LENGTH = 10;
+/**
+ * Binary search for item in an array. Returns index of some (not necessarily first) item.
+ * @param {Number} num - Number to search.
+ * @returns {Number} Index of item if found, `-1` otherwise.
+ */
+Array.prototype.binarySearch = function(num) {
+  let start = 0, end = this.length - 1, i;
+  while (start <= end) {
+    i = Math.floor(start / 2 + end / 2);
+    if (num === this[i]) {
+      return i;
+    }
+    if (num < this[i]) {
+      end = i - 1;
+    }
+    if (num > this[i]) {
+      start = i + 1;
+    }
+  }
+
+  return -1;
+};
+
+const MIN_INT_NUMBER = 0, MAX_INT_NUMBER = 10, ARRAY_LENGTH = 10;
 
 function getRandomInt(min, max) {
-  	return Math.floor(Math.random() * (max - min)) + min;
-}
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
-function createSortedIntNumbersArrayByLength(length){
-	var array = [];
-	var i = 0;
-	
-	while(i < length){
-		array.push(getRandomInt(MIN_INT_NUMBER, MAX_INT_NUMBER));
-		i++;
-	}
+function createSortedIntNumbersArrayByLength(length) {
+  let array = [], i = 0;
+  while (i < length) {
+    array.push(getRandomInt(MIN_INT_NUMBER, MAX_INT_NUMBER));
+    i++;
+  }
 
-	return array.sort(function(a, b){ return a - b; });
-}
+  return array.sort();
+};
 
-Array.prototype.recursiveBinarySearch = function(num){
-	var start = 0;
-	var end = this.length - 1;
-	var i;
-	while(start <= end){
-		i = Math.floor((start + end)/2);
-		if(num === this[i]){
-			return i;
-		}
-		if(num < this[i]){
-			end = i - 1;
-		}
-		if(num > this[i]){
-			start = i + 1;
-		}
-	}
+function main() {
+  let arr = createSortedIntNumbersArrayByLength(ARRAY_LENGTH),
+    num = getRandomInt(MIN_INT_NUMBER, MAX_INT_NUMBER),
+    i = arr.binarySearch(num);
 
-	return -1;
-}
-
-function main(){
-	var arr = createSortedIntNumbersArrayByLength(ARRAY_LENGTH);
-	var num = getRandomInt(MIN_INT_NUMBER, MAX_INT_NUMBER);
-	var i = arr.recursiveBinarySearch(num);
-
-	console.log(arr.join(' '));
-	console.log(num);
-	console.log(i);
-}
+  console.log((i === -1 && arr.indexOf(num) === -1) || num === arr[i]);
+};
